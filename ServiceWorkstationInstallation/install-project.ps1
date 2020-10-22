7ec5fc4276dee8ddcd48f3755080fe080a580d02
Colin Nash <colin.nash@orckestra.com>
2020-10-22 08:38:03 -0400

    Updated to most recent install scripts.

--- ServiceWorkstationInstallation/install-project.ps1
+++ ServiceWorkstationInstallation/install-project.ps1
@@ -1,5 +1,5 @@
 # Clone the repository
-git clone https://orckestra002.visualstudio.com/DefaultCollection/Hach/_git/Hach
+git clone https://dev.azure.com/orckestra002/Hach/_git/Hach
 
 # Checkout the develop branch
 cd Hach
@@ -9,11 +9,11 @@ git checkout develop
 nuget restore Hach.sln
 
 # MSBuild
-& 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe' Hach.sln
+& 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe' Hach.sln
 
 # npm install
 cd src/Hach.Website
-npm install
+npm install --force
 
 # gulp build
 gulp build
@@ -26,19 +26,22 @@ cd ../../build
 ./build.ps1 -t all
 
 # Create tools folder
-New-Item -ItemType directory -Path C:\tools
+if(-Not(Test-Path -Path C:\tools)){
+	New-Item -ItemType directory -Path C:\tools
+}
 
 # Add db_admin user with sysadmin role to SQL Server
 sqlcmd -Q "CREATE LOGIN db_admin WITH PASSWORD = 'P@ssw0rd'; EXEC sp_addsrvrolemember @loginame = 'db_admin', @rolename = 'sysadmin';"
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$ProgressPreference = 'SilentlyContinue'
-$url = "http://bin.orckestra.com/Assets.zip"
-		$output = "C:\tools\Assets.zip"
-		$start_time = Get-Date
-		Invoke-WebRequest -Uri $url -OutFile $output
-$ProgressPreference = 'Continue'
-[System.IO.Compression.ZipFile]::ExtractToDirectory($output, "c:\tools")
-
+if(-Not(Test-Path -Path "c:\tools\Assets" )){
+	Add-Type -AssemblyName System.IO.Compression.FileSystem
+	$ProgressPreference = 'SilentlyContinue'
+	$url = "http://bin.orckestra.com/Assets.zip"
+			$output = "C:\tools\Assets.zip"
+			$start_time = Get-Date
+			Invoke-WebRequest -Uri $url -OutFile $output
+	$ProgressPreference = 'Continue'
+	[System.IO.Compression.ZipFile]::ExtractToDirectory($output, "c:\tools")
+}
 # Integration
 ./Integration.ps1 -t all
